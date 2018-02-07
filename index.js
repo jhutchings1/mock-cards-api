418d3edb80efa89de4c93f51d22709fb270c16ab
Joel Wasserman <jwasserman@redventures.com>
2018-02-07 13:26:33 -0500

    adding in cors support

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const app = express()
 const routes = require('./routes')
 const path = require('path')
 const Ddos = require('ddos')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 
 // rate limit requests by ip
 const ddos = new Ddos({ burst: 10, limit: 15 })
+
+// allow cors
+app.use(cors())
 
 // body parse incoming reqs as json for convenience
 app.use(bodyParser.json())
