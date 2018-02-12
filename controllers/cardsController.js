078b5d180bbbba0318c569bb4efcf22168594104
Joel Wasserman <jwasserman@redventures.com>
2018-02-11 21:39:00 -0500

    udpate

--- controllers/cardsController.js
+++ controllers/cardsController.js
@@ -26,18 +26,22 @@ module.exports = {
     const usersCreditScore = req.body.creditScore
     const cardAppliedForId = req.params.id
 
+    // get card applied for
+    const cardAppliedForArray = staticJson.allCardInfo.filter(card => card.id === cardAppliedForId)
+
     // validate that the card applied for exists
-    if (!Object.keys(staticJson.allCardInfo).includes(cardAppliedForId)) {
+    if (cardAppliedForArray.length < 1) {
       return res.status(400).json({ success: false, message: 'No card matches the id passed in, make sure the id passed in is the integer value of the id for the card being applied for' })
     }
+
+    const cardAppliedFor = cardAppliedForArray[0]
 
     // validate the credit score passed in
     if (usersCreditScore < 100 || usersCreditScore > 850) {
       return res.status(400).json({ success: false, message: 'Invalid credit score passed in' })
     }
 
-    const cardAppliedFor = staticJson.allCardInfo[cardAppliedForId]
-    const minPotentialScore = cardAppliedFor.recommended_credit_scores.reduce((min, score) => Math.min(min, score.min), 0)
+    const minPotentialScore = cardAppliedFor.recommended_credit_scores.reduce((min, score) => Math.min(min, score.min), 1000)
 
     // check if the user has a valid credit score, anything above the min will result in approval
     if (usersCreditScore >= minPotentialScore) {
